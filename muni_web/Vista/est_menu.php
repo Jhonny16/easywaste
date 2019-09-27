9b7d398731e399d327979e6523b97e759515f1c5
jhonny16 <tinny.1609@gmail.com>
2019-09-27 13:14:27 -0500

    subiendo web

---
+++ muni_web/Vista/est_menu.php
@@ -0,0 +1,60 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="../imagenes/user.png" class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+                <p id="menu_user"></p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Activo</a>
+            </div>
+        </div>
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+
+        </form>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">MENÚ DESPLEGABLE</li>
+            <li class="treeview" >
+                <a href="#">
+                    <img src="../imagenes/perfil.png" style="width: 1.5em" alt="">
+                    <span style="color: #01a189">Mi Perfil</span>
+                </a>
+            </li>
+            <li class="treeview" id="menu_recicladores">
+                <a href="#">
+                    <img src="../imagenes/trash.jpg" style="width: 1.5em" alt="">
+                    <span style="color: #01a189">Reciclador</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="reciclador.php" ><i class="fa fa-circle-o"></i> Nuevo</a></li>
+                    <li><a href="reciclador_list.php"><i class="fa fa-circle-o"></i> Lista</a></li>
+                </ul>
+            </li>
+            <li class="treeview" id="menu_proveedores">
+                <a href="#">
+                    <img src="../imagenes/persona.png" style="width: 1.5em" alt="">
+                    <span style="color: #01a189">Proveedor</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="" ><i class="fa fa-circle-o"></i> Nuevo</a></li>
+                    <li><a href=""><i class="fa fa-circle-o"></i> Lista</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
+<!-- Content Wrapper. Contains page content -->
